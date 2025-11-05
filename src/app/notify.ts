9c6977a3375e2562eeefb6d5cd1c2266b1a3a9be
AndikaPrsty <andikaandika866@gmail.com>
2025-11-05 22:48:30 +0700

    enhance notification

--- src/app/notify.ts
+++ src/app/notify.ts
@@ -1,13 +1,19 @@
 import { NotifySend } from "node-notifier";
+import os from "os";
+import { exec } from "child_process";
 
 
 const notifier = new NotifySend();
 
 export const notify = (title = "", message = "", urgency: "" | "warning" | "critical" = undefined) => {
-  notifier.notify({
-    title,
-    message,
-    urgency
-  })
+  if (os.platform() === "android") {
+    exec(`termux-notification --title "${title}" --content "${message}" --sound`)
+  } else {
+    notifier.notify({
+      title,
+      message,
+      urgency
+    })
+  }
 }
 
