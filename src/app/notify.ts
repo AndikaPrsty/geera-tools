08dda512daec424b78faf4206ad974f2d05a96d4
AndikaPrsty <andikaandika866@gmail.com>
2025-11-07 09:50:13 +0700

    add vibrate notif for android

--- src/app/notify.ts
+++ src/app/notify.ts
@@ -7,7 +7,8 @@ const notifier = new NotifySend();
 
 export const notify = (title = "", message = "", urgency: "" | "warning" | "critical" = undefined) => {
   if (os.platform() === "android") {
-    exec(`termux-notification --title "${title}" --content "${message}" --sound`)
+    exec(`termux-notification --title "${title}" --content "${message}" --sound`);
+    exec("termux-vibrate -d 3000");
   } else {
     notifier.notify({
       title,
